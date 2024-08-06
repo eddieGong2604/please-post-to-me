0f7faa847cbf1fcc9de02823a19ab4bbc4908cbf
eddy <eddy.l@aihomegroup.com.au>
2024-08-06 17:06:12 +1000

    mock data

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -4,11 +4,11 @@ import mockCustomers from "../data/mockCustomers.js";
 
 const router = express.Router();
 
-router.post('/:id/details', (req, res) => {
+router.get('/:id/details', (req, res) => {
     res.json({ vehicle: mockVehicleDetails });
 });
 
-router.post('/:id/customers', (req, res) => {
+router.get('/:id/customers', (req, res) => {
     res.json({ customers: mockCustomers });
 });
 
