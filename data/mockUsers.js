e73b26709e53f1260b8edc5f11a016a9b5ef81f3
Liam263 <nguyenthanhlam091@gmail.com>
2024-08-22 18:02:14 +1000

    feat add GET: /user-me

--- data/mockUsers.js
+++ data/mockUsers.js
@@ -1,7 +1,16 @@
-
 const mockUsers = [
-    { username: 'user1', password: 'password1' },
-    { username: 'user2', password: 'password2' }
+  {
+    username: "user1",
+    password: "password1",
+    firstName: "user1",
+    lastName: "nguyen",
+  },
+  {
+    username: "user2",
+    password: "password2",
+    firstName: "user2",
+    lastName: "tran",
+  },
 ];
 
 export default mockUsers;
---
+++ routes/user-me.js
@@ -0,0 +1,22 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import mockUsers from "../data/mockUsers.js";
+import "dotenv/config";
+import middleware from "../middleware/index.js";
+const router = express.Router();
+
+router.get("/", middleware.authWeb, (req, res) => {
+
+    const parts = req.headers.authorization.split(" ");
+    const decoded = jwt.decode(parts[1], process.env.JWT_SECRET)
+    
+    const user = mockUsers.find(user => user.username === decoded.username)
+    if(!user){
+        return res.status(404).json({ message: "User not found" });
+    }
+  res.json({ firstName: user.firstName, 
+    lastName: user.lastName });
+});
+
+
+export default router
